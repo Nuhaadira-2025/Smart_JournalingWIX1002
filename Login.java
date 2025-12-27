02dd69791eeab24465c05a1c2f9155dc9214744c
Nuha Adira Binti Mohd Ridzuan <23111467@siswa.um.edu.my>
2025-12-27 19:59:31 +0800

    Refactor Login class and add PasswordSecurity

--- Login.java
+++ Login.java
@@ -95,6 +95,40 @@ static void login() {
     }
 }
 
+
+
+public class PasswordSecurity {
+
+    
+    private static final int SHIFT = 3;
+
+    
+    public static String encrypt(String password) {
+        if (password == null) return null;
+        
+        StringBuilder encrypted = new StringBuilder();
+        for (char character : password.toCharArray()) {
+            // Shift the character by 3
+            encrypted.append((char) (character + SHIFT));
+        }
+        return encrypted.toString();
+    }
+
+    
+    public static String decrypt(String encryptedPassword) {
+        if (encryptedPassword == null) return null;
+
+        StringBuilder decrypted = new StringBuilder();
+        for (char character : encryptedPassword.toCharArray()) {
+            // Shift the character back by 3
+            decrypted.append((char) (character - SHIFT));
+        }
+        return decrypted.toString();
+    }
+    
+
+}
+}
     
 
 
