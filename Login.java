d8cac4dd5f4b71befb8a5c2508e321f4f8af34ae
Nuha Adira Binti Mohd Ridzuan <23111467@siswa.um.edu.my>
2025-12-30 00:00:07 +0800

    Refactor Login class and remove redundant code

--- Login.java
+++ Login.java
@@ -97,38 +97,8 @@ static void login() {
 
 
 
-public class PasswordSecurity {
-
-    
-    private static final int SHIFT = 3;
-
-    
-    public static String encrypt(String password) {
-        if (password == null) return null;
-        
-        StringBuilder encrypted = new StringBuilder();
-        for (char character : password.toCharArray()) {
-            // Shift the character by 3
-            encrypted.append((char) (character + SHIFT));
-        }
-        return encrypted.toString();
-    }
-
-    
-    public static String decrypt(String encryptedPassword) {
-        if (encryptedPassword == null) return null;
-
-        StringBuilder decrypted = new StringBuilder();
-        for (char character : encryptedPassword.toCharArray()) {
-            // Shift the character back by 3
-            decrypted.append((char) (character - SHIFT));
-        }
-        return decrypted.toString();
-    }
-    
 
 }
-}
     
 
 
@@ -137,6 +107,7 @@ public class PasswordSecurity {
     
 
     
+
 
 
 
