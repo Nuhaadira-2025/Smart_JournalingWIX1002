2a64ec6984e6fb6b54a77574ec74c9655881326e
Nuha Adira Binti Mohd Ridzuan <23111467@siswa.um.edu.my>
2026-01-09 23:24:46 +0800

    Add JournalEntry class with properties and methods

--- journalEntry.java
+++ journalEntry.java
@@ -1,1 +1,19 @@
+package smartjournaling;
 
+public class JournalEntry {
+    private String email, date, content, mood, weather;
+
+    public JournalEntry(String email, String date, String content, String mood, String weather) {
+        this.email = email;
+        this.date = date;
+        this.content = content;
+        this.mood = mood;
+        this.weather = weather;
+    }
+
+    public String getEmail() { return email; }
+    public String getDate() { return date; }
+    public String getContent() { return content; }
+    public String getMood() { return mood; }
+    public String getWeather() { return weather; }
+}
